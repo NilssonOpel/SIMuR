942855b8642ba23a318ece9538ac482f8a0e2b8f
Hans T Walheim <hans.walheim@gmail.com>
2020-01-18 09:22:50 +0100

    Some convenience stuff for cleaning

--- test/clean.bat
+++ test/clean.bat
@@ -5,5 +5,10 @@ rd /s /q build
 rd /s /q %ROOT%
 rd /s /q hidden_%ROOT%
 
-:: and maby also
+:: and maybe also
 rd /s /q %LOCALAPPDATA%\Sourceserver
+:: Will remove VS's own internal SRCSRV cache
+
+:: but may not
+:: rd /s /q %SIMUR_REPO_CACHE%
+:: Since that will remove you current cache of the repos
