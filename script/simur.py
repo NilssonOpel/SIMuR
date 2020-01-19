39d118a7a7d9b2a9a344a6ada35114931681e391
Hans T Walheim <hans.walheim@gmail.com>
2020-01-19 08:58:25 +0100

    Add getFileProperties (from StackOverflow)

--- script/simur.py
+++ script/simur.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess
+import win32api
 
 #-------------------------------------------------------------------------------
 #
@@ -141,3 +142,48 @@ def find_and_update_git(local_repo, reporoot):
     os.chdir(curr_dir)
 
     return git_dir
+
+import win32api
+
+#-------------------------------------------------------------------------------
+# Shamelessly stolen from:
+# https://stackoverflow.com/questions/580924/python-windows-file-version-attribute
+#-------------------------------------------------------------------------------
+def getFileProperties(fname):
+    """
+    Read all properties of the given file return them as a dictionary.
+    """
+    propNames = ('Comments', 'InternalName', 'ProductName',
+        'CompanyName', 'LegalCopyright', 'ProductVersion',
+        'FileDescription', 'LegalTrademarks', 'PrivateBuild',
+        'FileVersion', 'OriginalFilename', 'SpecialBuild')
+
+    props = {'FixedFileInfo': None, 'StringFileInfo': None, 'FileVersion': None}
+
+    try:
+        # backslash as parm returns dictionary of numeric info
+        # corresponding to VS_FIXEDFILEINFO struc
+        fixedInfo = win32api.GetFileVersionInfo(fname, '\\')
+        props['FixedFileInfo'] = fixedInfo
+        props['FileVersion'] = "%d.%d.%d.%d" % (fixedInfo['FileVersionMS'] / 65536,
+                fixedInfo['FileVersionMS'] % 65536, fixedInfo['FileVersionLS'] / 65536,
+                fixedInfo['FileVersionLS'] % 65536)
+
+        # \VarFileInfo\Translation returns list of available (language, codepage)
+        # pairs that can be used to retreive string info. We are using only the first pair.
+        lang, codepage = win32api.GetFileVersionInfo(fname, '\\VarFileInfo\\Translation')[0]
+
+        # any other must be of the form \StringfileInfo\%04X%04X\parm_name, middle
+        # two are language/codepage pair returned from above
+
+        strInfo = {}
+        for propName in propNames:
+            strInfoPath = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage, propName)
+            ## print str_info
+            strInfo[propName] = win32api.GetFileVersionInfo(fname, strInfoPath)
+
+        props['StringFileInfo'] = strInfo
+    except:
+        pass
+
+    return props
